96c5e35b0baf7751ab70026e51a5854fdfce267c
kishkevich <@Anfiar>
2024-01-24 17:32:07 +0300

    refactor name of methods

--- src/main/java/pages/JustJoinItPage.java
+++ src/main/java/pages/JustJoinItPage.java
@@ -116,7 +116,6 @@ public class JustJoinItPage {
         //WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         //wait.until(ExpectedConditions.elementToBeClickable(By.xpath(topCompaniesButton)));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-        driver.findElement(By.xpath(candidateProfileSignIn));
         driver.findElement(By.xpath(topCompaniesButton)).click();
         //new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOfElementLocated(By.xpath(topCompaniesButton))).click();
         //new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(By.xpath(topCompaniesButton)));
