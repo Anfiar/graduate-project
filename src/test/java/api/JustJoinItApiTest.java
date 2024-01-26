4ace5d2ebd36439e441ae7b95b13c8d01f1703c2
kishkevich <@Anfiar>
2024-01-26 17:04:48 +0300

    Refactor name of methods in JustJoinItApiTest

--- src/test/java/api/JustJoinItApiTest.java
+++ src/test/java/api/JustJoinItApiTest.java
@@ -5,91 +5,91 @@ import org.junit.jupiter.api.Test;
 
 public class JustJoinItApiTest extends BaseApiTest {
     @Test
-    public void testLogin1() {
+    public void testLogin() {
         String requestBody = String.valueOf(JsonBodyGenerator.getRandomJsonByEmailAndPassword());
         int statusCode = 401;
         loginMethod(requestBody, statusCode);
     }
 
     @Test
-    public void testLogin2() {
+    public void testLoginWithAdditionalField() {
         String requestBody = String.valueOf(JsonBodyGenerator.getRandomJsonWithAdditionalField());
         int statusCode = 401;
         loginMethod(requestBody, statusCode);
     }
 
     @Test
-    public void testLogin3() {
+    public void testLoginWithEmptyBody() {
         String requestBody = "";
         int statusCode = 415;
         loginMethod(requestBody, statusCode);
     }
 
     @Test
-    public void testLogin4() {
+    public void testLoginWithEmptyJsonBody() {
         String requestBody = String.valueOf(JsonBodyGenerator.getEmptyRandomJson());
         int statusCode = 422;
         loginMethod(requestBody, statusCode);
     }
 
     @Test
-    public void testLogin5() {
+    public void testLoginOnlyWithPassword() {
         String requestBody = String.valueOf(JsonBodyGenerator.getRandomJsonByFields("password"));
         int statusCode = 422;
         loginMethod(requestBody, statusCode);
     }
 
     @Test
-    public void testLogin6() {
+    public void testLoginWithWrongEmailAndPassword() {
         String requestBody = String.valueOf(JsonBodyGenerator.getRandomJsonWithIncorrectValueOfField());
         int statusCode = 401;
         loginMethod(requestBody, statusCode);
     }
 
     @Test
-    public void testLogin7() {
+    public void testLoginWithIntegerEmailValue() {
         String requestBody = String.valueOf(JsonBodyGenerator.getRandomJsonWithIntegerValueOfField("email"));
         int statusCode = 415;
         loginMethod(requestBody, statusCode);
     }
 
     @Test
-    public void testLogin8() {
+    public void testLoginWithIntegerValueOfField() {
         String requestBody = String.valueOf(JsonBodyGenerator.getRandomJsonWithIntegerValueOfField("both"));
         int statusCode = 415;
         loginMethod(requestBody, statusCode);
     }
 
     @Test
-    public void testLogin9() {
+    public void testLoginWithNullValueOfField() {
         String requestBody = String.valueOf(JsonBodyGenerator.getRandomJsonWithNullValueOfField());
         int statusCode = 422;
         loginMethod(requestBody, statusCode);
     }
 
     @Test
-    public void testLogin10() {
+    public void testLoginOnlyWithEmail() {
         String requestBody = String.valueOf(JsonBodyGenerator.getRandomJsonByFields("email"));
         int statusCode = 422;
         loginMethod(requestBody, statusCode);
     }
 
     @Test
-    public void testLogin11() {
+    public void testLoginWithEmptyValue() {
         String requestBody = String.valueOf(JsonBodyGenerator.getRandomJsonWithSomeEmptyValueOfField("both"));
         int statusCode = 422;
         loginMethod(requestBody, statusCode);
     }
 
     @Test
-    public void testLogin12() {
+    public void testLoginWithEmptyPassword() {
         String requestBody = String.valueOf(JsonBodyGenerator.getRandomJsonWithSomeEmptyValueOfField("password"));
         int statusCode = 422;
         loginMethod(requestBody, statusCode);
     }
 
     @Test
-    public void testLogin13() {
+    public void testLoginWithEmptyEmail() {
         String requestBody = String.valueOf(JsonBodyGenerator.getRandomJsonWithSomeEmptyValueOfField("email"));
         int statusCode = 422;
         loginMethod(requestBody, statusCode);
