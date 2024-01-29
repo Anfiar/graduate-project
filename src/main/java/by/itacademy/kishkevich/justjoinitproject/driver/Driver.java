d95ba7f7ceaa0267a8eaed256854a10908088e48
kishkevich <@Anfiar>
2024-01-29 14:51:19 +0300

    Refactoring

--- src/main/java/by/itacademy/kishkevich/justjoinitproject/driver/Driver.java
+++ src/main/java/by/itacademy/kishkevich/justjoinitproject/driver/Driver.java
@@ -3,6 +3,7 @@ package by.itacademy.kishkevich.justjoinitproject.driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -10,10 +11,12 @@ import java.time.Duration;
 
 public class Driver {
     private static WebDriver driver;
+    private static ChromeOptions options;
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            driver = new ChromeDriver();
+            setUpChromeOptions();
+            driver = new ChromeDriver(options);
             setUp();
         }
         return driver;
@@ -24,6 +27,13 @@ public class Driver {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
     }
+
+    private static void setUpChromeOptions() {
+        options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--disable-gpu");
+    }
+
     public static void waiter(By xpath) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.presenceOfElementLocated(xpath));
